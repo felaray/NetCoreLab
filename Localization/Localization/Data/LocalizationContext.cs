5bd52ae1e79ad6cf535013eaab60911f04513938
Felaray Ho <vivatn@msn.com>
2020-10-23 01:54:01 +0800

    Init

---
+++ Localization/Localization/Data/LocalizationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Localization.Models;
+
+namespace Localization.Data
+{
+    public class LocalizationContext : DbContext
+    {
+        public LocalizationContext (DbContextOptions<LocalizationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Localization.Models.Test> Test { get; set; }
+    }
+}
---
+++ Localization/Localization/Program.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Localization.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace Localization
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            CreateDbIfNotExists(host);
+
+            host.Run();
+        }
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+
+
+                var context = services.GetRequiredService<LocalizationContext>();
+
+#if DEBUG
+                //context.Database.EnsureDeleted();
+#endif
+                //context.Database.EnsureCreated();
+                context.Database.Migrate();
+
+
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+}
